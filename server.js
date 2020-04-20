d178e88a04cfce784bfb139580f3c4f77d06f6fb
Ezequiel Leanes <ezequiel.leanes@gmail.com>
2020-04-20 14:05:06 -0300

    T1 and T2 finished

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
+/*Username: Sf4Xe2cR6N
+
+Database name: Sf4Xe2cR6N
+
+Password: PdOw1gkbVO
+
+Server: remotemysql.com
+
+Port: 3306*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
 const server = express();
 
 let productos = [
@@ -7,25 +19,53 @@ let productos = [
         id : 1,
         nombre : 'Hamburguesa',
         precio : 350,
-        url : ''
+        url : '',
+        isFavorite : true
     },
     {
         id : 2,
         nombre : 'Lomito',
         precio : 400,
-        url : ''
+        url : '',
+        isFavorite : false
     },
     {
         id : 3,
         nombre : 'Papas Fritas',
         precio : 200,
-        url : ''
+        url : '',
+        isFavorite : true
     }
+]
+
+const claveSegura = 'delahila'
+
+let usuarios = [
+    {
+        usuario : 'Ezequiel',
+        contrasena : 'Password'
+    },
+    {
+        usuario : 'Martin',
+        contrasena : '12345678'
+    }
+
 ]
 
 server.listen(3000, () => console.log('Servidor iniciado...'));
 
+
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(cors());
+
+// Cors
+
+function allowCors(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+};
 
 //Middlewares
 
@@ -40,6 +80,9 @@ function productValidator(req, res, next){
         if(match) {
             res.status(409).json('El id ya existe');
         } else {
+            req.body.id = parseInt(req.body.id);
+            req.body.precio = parseInt(req.body.precio);
+            req.body.isFavorite === 'true' ? req.body.isFavorite = true : req.body.isFavorite = false;
             next();
         }       
     } else {
@@ -47,11 +90,52 @@ function productValidator(req, res, next){
     }  
 };
 
-server.get('/productos', (req, res) => {
+function userPassValidator(usuario, contrasena) {
+    const [filterUser] = usuarios.filter(element => element.usuario === usuario && element.contrasena === contrasena);
+    if (!filterUser) {
+        return false;
+    }
+    return filterUser;
+};
+
+function userAuthenticaton(req, res, next) {
+    try {
+        const token = req.headers.authorization.split(' ')[1];
+        const verifyToken = jwt.verify(token, claveSegura);
+        if (verifyToken) {
+            req.usuario = verifyToken;
+            return next();
+        }
+    } catch (err) {
+        res.json({ error: 'Error al validar el usuario'});
+    }
+}
+
+//Paths
+
+server.post('/login', allowCors, (req, res) => {
+    const { usuario, contrasena } = req.body;
+    const validated = userPassValidator(usuario, contrasena);
+    if (!validated) {
+        res.json({error: 'No existe el usuario o la contraseña es incorrecta'});
+        return
+    }
+
+    const token = jwt.sign({usuario}, claveSegura);
+
+    res.json({ token })
+
+});
+
+server.get('/auth', [userAuthenticaton, allowCors], (req, res) => {
+    res.send(`Esta es una página autenticada. Hola ${req.usuario.usuario} !`);
+})
+
+server.get('/productos', allowCors, (req, res) => {
     res.json(productos);
 });
 
-server.get('/productos/:idproducto', (req, res) => {
+server.get('/productos/:idproducto', allowCors, (req, res) => {
     const idproducto = req.params.idproducto;
     if(idproducto > productos.length || idproducto == 0) {
         return res.status(404).send('Artículo no encontrado');
@@ -59,8 +143,20 @@ server.get('/productos/:idproducto', (req, res) => {
     res.json(productos[idproducto-1]);
 });
 
-server.post('/nuevo-producto', productValidator, (req, res) => {
+server.post('/productos', [productValidator, allowCors], (req, res) => {
     productos.push(req.body);
     console.log('El producto ha sido agregado con exito');
+    console.log(productos);
     res.json(req.body);
-})
+});
+
+server.get('/productos-favoritos', allowCors, (req, res) => {
+    const favorites = productos.filter(elem => elem.isFavorite == true);
+    console.log(favorites);
+    res.json(favorites);
+    
+});
+
+/* server.post('/register' (req, res) => {
+
+}) */
